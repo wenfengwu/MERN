b843cbb379f4c1aa15b2fb32b7a755cdfb3fe35f
Wenfeng(Kevin) Wu <wenfengwu92@gmail.com>
2021-10-25 14:37:21 -0700

    10.25

--- Practice Assignment/superNinja.js
+++ Practice Assignment/superNinja.js
@@ -23,6 +23,7 @@ class Ninja{
 class Sensei extends Ninja{
     constructor(name){
         super(name, 200, 10, 10);
+        this.wisdom = 10;
     }
     speakWisdom(){
         console.log("What one programmer can do in one month, two programmers can do in two months.")
