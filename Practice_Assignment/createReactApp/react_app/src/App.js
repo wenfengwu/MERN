78385e9d33e239fb31f374a2b401f00919dd1927
Wenfeng(Kevin) Wu <wenfengwu92@gmail.com>
2021-10-26 21:51:50 -0700

    10.26

--- Practice_Assignment/createReactApp/react_app/src/App.js
+++ Practice_Assignment/createReactApp/react_app/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marthon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </>
   );
 }
 
