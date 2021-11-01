97cbe8ee4544758a7fb82e4c3de00688a8dc8ae8
Wenfeng(Kevin) Wu <wenfengwu92@gmail.com>
2021-11-01 16:06:44 -0700

    11.01

---
+++ Practice_Assignment/CoinFlipping.js
@@ -0,0 +1,33 @@
+function tossCoin() {
+        return Math.random() > 0.5 ? "heads" : "tails";
+    }
+
+function fiveHeads() {
+        return new Promise( (resolve, reject) => {
+            let headsCount = 0;
+            let attempts = 0;
+            while(headsCount < 5) {
+                attempts++;
+                let result = tossCoin();
+                console.log(`${result} was flipped`);
+                if(result === "heads") {
+                    headsCount++;
+                } else {
+                    headsCount = 0;
+                }
+            }
+
+            if(attempts >= 100){
+                reject("Flipped more than 10 times, game quits")
+            }
+            else{
+                resolve(`It took ${attempts} tries to flip five "heads"`)
+            }
+        });
+    }
+
+fiveHeads()
+    .then( res => console.log(res) )
+    .catch( err => console.log(err) );
+console.log( "When does this run now?" );
+        
